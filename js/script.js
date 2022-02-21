9f5e6eeebad352a810366df131be79d8df841cb4
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-02-21 23:31:09 +0100

    21.02

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const apiUrl = "https://iata-and-icao-codes.p.rapidapi.com/airlines";
+
+const rapidApiKey = {
+  headers: {
+    "x-rapidapi-key": "e360b3c142msh1d6f915ac4d91e2p115aedjsn35105dc71911"
+  },
+};
+
+let loader = document.querySelector(".loader")
+
+
+async function getAirData() {
+  const response = await fetch(apiUrl, rapidApiKey);
+  const json = await response.json();
+  console.log(json);
+
+  loader.innerHTML = "";  
+
+}
+
+
+
+getAirData();
+
+
