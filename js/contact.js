fcba06adf672fe687648709d3e6c396780ec11b8
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-02-22 14:41:05 +0100

    22.02

---
+++ js/contact.js
@@ -0,0 +1,46 @@
+const form = document.querySelector("#contactForm");
+const nameInput = document.querySelector("#name");
+const nameError = document.querySelector("#nameError");
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const adress = document.querySelector("#adress");
+const adressError = document.querySelector("#adressError");
+const validForm = document.querySelector(".validForm");
+
+function validateForm(event) {
+  event.preventDefault();
+
+  if (nameInput.value.trim().length > 0) {
+    nameError.style.display = "none";
+  } else {
+    nameError.style.display = "block";
+  }
+
+  if (subject.value.trim().length > 9) {
+    subjectError.style.display = "none";
+  } else {
+    subjectError.style.display = "block";
+  }
+
+  if (validateEmail(email.value) === true) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+
+  if (adress.value.trim().length > 24) {
+    adressError.style.display = "none";
+  } else {
+    adressError.style.display = "block";
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/;
+  const matchPattern = regEx.test(email);
+  return matchPattern;
+}
+
+form.addEventListener("submit", validateForm);
--- js/script.js
+++ js/script.js
@@ -2,24 +2,18 @@ const apiUrl = "https://iata-and-icao-codes.p.rapidapi.com/airlines";
 
 const rapidApiKey = {
   headers: {
-    "x-rapidapi-key": "e360b3c142msh1d6f915ac4d91e2p115aedjsn35105dc71911"
+    "x-rapidapi-key": "e360b3c142msh1d6f915ac4d91e2p115aedjsn35105dc71911",
   },
 };
 
-let loader = document.querySelector(".loader")
-
+let loader = document.querySelector(".loader");
 
 async function getAirData() {
   const response = await fetch(apiUrl, rapidApiKey);
   const json = await response.json();
   console.log(json);
 
-  loader.innerHTML = "";  
-
+  loader.innerHTML = "";
 }
 
-
-
 getAirData();
-
-
